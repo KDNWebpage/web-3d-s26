d4b6552bb949f3efd2c5c7454dde436b50d3ee63
Travis35837 <jcdsjyd6rx@privaterelay.appleid.com>
2026-02-19 16:14:24 -0500

    Project Work

--- MindNotes/sketch.js
+++ MindNotes/sketch.js
@@ -1,18 +1,20 @@
 let backgroundHue = 250;
+let myShape;
 
 var s1 = function( sketch ) {
+  sketch.preload = function() {
+    myShape=loadModel('./HumanModel.obj', true);
+  };
   sketch.setup = function() {
     let canvas1 = sketch.createCanvas(200, 250, sketch.WEBGL);
     canvas1.position(475,100);
-  }
+  };
   sketch.draw = function() {
     //for canvas 1
     sketch.background(100);
-    sketch.rotateX(sketch.frameCount * 0.01);
-    sketch.rotateZ(sketch.frameCount * 0.01);
-    sketch.cone(30, 50);
-  }
+    sketch.orbitControl();
+    sketch.model(myShape);
+  };
 };
 
-// create a new instance of p5 and pass in the function for sketch 1
 new p5(s1);
