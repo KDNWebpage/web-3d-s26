cf20ca45e680f75683e311e59fa7a5a1c28f29b5
Travis35837 <jcdsjyd6rx@privaterelay.appleid.com>
2026-02-23 11:22:47 -0500

    project

--- MindNotes/sketch.js
+++ MindNotes/sketch.js
@@ -1,19 +1,40 @@
 let backgroundHue = 250;
-let myShape;
 
 var s1 = function( sketch ) {
+  let humanBody;
+  let humanFace;
+  let chip;
+    
   sketch.preload = function() {
-    myShape=loadModel('./HumanModel.obj', true);
+    humanBody=sketch.loadModel('./HumanBody.obj', true);
+    humanFace=sketch.loadModel('./Face.obj', true);
+    chip=sketch.loadModel('./Chip.obj', true);
   };
   sketch.setup = function() {
     let canvas1 = sketch.createCanvas(200, 250, sketch.WEBGL);
     canvas1.position(475,100);
+    sketch.angleMode(sketch.DEGREES);
   };
   sketch.draw = function() {
     //for canvas 1
     sketch.background(100);
     sketch.orbitControl();
-    sketch.model(myShape);
+    sketch.rotateX(180);
+    sketch.noStroke();
+    sketch.fill(188, 190, 194);
+    sketch.model(humanBody);
+    sketch.scale(0.4);
+    sketch.translate(0,110,-60);
+    sketch.fill(0,0,0);
+    sketch.model(humanFace);
+    sketch.fill(43, 115, 255);
+    sketch.translate(-90,40,10);
+    sketch.rotateX(-180);
+    sketch.scale(0.3);
+    sketch.model(chip);
+
+   
+    
   };
 };
 
